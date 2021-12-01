e4715a0fdb6c6502b9fb0a58dfc78441730e8926
Dan Wright <wrightdj99@gmail.com>
2021-12-01 13:22:35 -0600

    initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Client extends JFrame{
+
+    private JTextField userText;
+    private JTextArea chatWindow;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    private String message = "";
+    private String serverIP;
+    private Socket connection;
+
+    public Client(String host){
+
+    }
+
+}
