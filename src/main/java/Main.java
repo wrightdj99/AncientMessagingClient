e529f3b0a28d195f5cb090797943d8536002281d
Dan Wright <wrightdj99@gmail.com>
2021-12-01 18:53:45 -0600

    Adding a JAR file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args){
+        //You'd enter in your host addr here
         Client mainClient = new Client("127.0.0.1");
         mainClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainClient.startRunning();
