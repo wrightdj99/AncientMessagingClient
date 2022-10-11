916265397a549451739a7b2c9f729b894e91c4ed
Dan Wright <wrightdj99@gmail.com>
2022-10-10 21:23:18 -0500

    Now you can run on two PCs!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import javax.swing.JFrame;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String addr;
         //You'd enter in your host addr here
-        Client mainClient = new Client("127.0.0.1");
+        System.out.println("Find out the IPv4 Address of your server machine and enter it here:\n");
+        addr = scan.nextLine();
+        Client mainClient = new Client(addr);
         mainClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainClient.startRunning();
     }
